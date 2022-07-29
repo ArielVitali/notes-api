df14b4ec59c21b75b6b5018897b2f5876e0e378e
arielvitali <arielgonzalezvitali@hotmail.com>
2022-07-29 10:33:17 -0300

    update

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
     {
-        "id":"1",
-        "impotant":"true"
+        "id":1,
+        "nombre":"Ariel",
+        "impotant":true
     },
     {
-        "id":"2",
-        "important":"false"
+        "id":2,
+        "nombre":"Gonza",
+        "important":false
     },
     {
-        "id":'3',
-        "important":'false'
+        "id":3,
+        "nombre":"Pedro",
+        "important":false
     }
 ]
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200,{'Content-Type':'application/json'})
-    res.end(JSON.stringify(notes))
+app.get('/',(req,res) =>{
+    res.send('<h1>hola mundo <h1>')
 })
 
-app.listen(3000)
+app.get('/api/notes', (req,res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req,res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+    console.log({note})
+    res.json(note)
+})
+
+
+
+
+
+
+const PORT = 3000
+app.listen(3000, () => {
+    console.log(`Server running on port ${PORT}...`)
+})
